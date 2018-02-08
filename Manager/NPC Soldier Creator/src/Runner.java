2ff973dc8a8e9d533a95172ba1ca22158de093ff
acocanower <acocanower@yahoo.com>
2018-02-07 19:39:59 -0500

    Creating test of Generating Soldiers
    Creating test of Generating Soldiers

---
+++ Manager/NPC Soldier Creator/src/Runner.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ryan
+ *
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Soldier test = new Soldier();
+	}
+
+}
