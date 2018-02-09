78d6b42182387d2b55b4c0fa9d581029ed4c42d1
acocanower <acocanower@yahoo.com>
2018-02-09 10:59:43 -0500

    Soldier Maker GUI update
    Made a GUI for generating Soldiers, this will (in the future) feed into a JFrame with an array of the soldiers on the screen with text fields and buttons to deal damage and display stats

--- Manager/NPC Soldier Creator/src/Runner.java
+++ Manager/NPC Soldier Creator/src/Runner.java
@@ -5,15 +5,47 @@
  *
  */
 //Imports
-//User interface
+//GUI
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Container;
+
+//listeners
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//command line interface and parsing
 import java.util.Scanner;
 
-public class Runner {
+
+public class Runner extends JFrame 
+implements ActionListener {
+	
+	//Interface
+	private JButton BTNcreateSoldiers;
+	private JTextField TXFnumberOfSoldiers;
+	private JLabel LBLheader;
+	private Container pane;
+	
+	//JFrame constants
+	private String WINDOW_TITLE = "Soldier Generator";
+    private final int WIDTH=400;
+    private final int HEIGTH=200;
+
+	/**
+	 * Autogened because I have no idea what it is....
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Runner run = new Runner();		
+		
+		/**
 		//Loads keyboard
 		Scanner keyboard = new Scanner(System.in);
 		//
@@ -53,6 +85,57 @@ public class Runner {
 		test = new Soldier(name.generateRandomName());
 		test.kill();
 		test = new Soldier(name.generateRandomName());*/
+	}
+	
+	public Runner() {
+
+		//load name generator once to less processing
+		Names name = new Names();
+		
+		//Initialing fields and buttons and stuff
+		BTNcreateSoldiers = new JButton("Generate Soldiers");
+		TXFnumberOfSoldiers = new JTextField("1");
+		LBLheader = new JLabel("Enter Number of Soldiers to Generate",0);
+		
+		//making Jframe
+		setTitle("Soldier Generator");
+		setSize(WIDTH,HEIGTH);
+		setResizable(false);
+		
+		//sets layout
+		pane=getContentPane();
+		pane.setLayout(null);
+		
+		//sets size and location for the Label
+		LBLheader.setSize(WIDTH,30);
+		LBLheader.setLocation(0, 0);
+		
+		// sets size and location for the number of soldiers field
+		TXFnumberOfSoldiers.setSize(WIDTH/2,30);
+		TXFnumberOfSoldiers.setLocation(WIDTH/4,HEIGTH/4);
+
+		// sets size and location for the go button
+		BTNcreateSoldiers.setSize(WIDTH/2,30);//sets size for buttons
+		BTNcreateSoldiers.setLocation(WIDTH/4,HEIGTH/2);//bee do bee doo bee bee doo smooth jazz is the best, see above for comment but for button
+		
+		//add it all to the Jframe
+		pane.add(LBLheader);
+		pane.add(TXFnumberOfSoldiers);
+		pane.add(BTNcreateSoldiers);
+		
+		//makes it so clicking the button triggers something to happen
+		BTNcreateSoldiers.addActionListener(this);
+		
+		//makes it so you can see the window
+		setVisible(true);
+	
+		//Not _really_ needed from my understanding but a good pratice
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println(TXFnumberOfSoldiers.getText());
 	}
 
 }
---
+++ Manager/NPC Soldier Creator/src/SoldierManagementGUI.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+/**
+ * @author Aaron
+ * 
+ * Last Edit: 2-9-2018 Aaron
+ *
+ */
+public class SoldierManagementGUI {
+
+}
